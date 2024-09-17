ceecda4608ae3104c15d337157f83156f0d1ef09
Cory Coward <ccoward80@gmail.com>
2024-09-16 20:44:42 -0400

    Add Startup class

---
+++ backend/Startup.cs
@@ -0,0 +1,35 @@
+namespace WebAppBackendTemplate;
+
+public class Startup
+{
+    private readonly IConfiguration _config;
+
+    public Startup(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
